98c6523179ab3d8df572572d051558e2ad30f738
amond lee <amond@amond.net>
2013-12-12 13:56:41 +0900

    add ConfirmValidate Constructor + custom message

--- library/src/com/throrinstudio/android/common/libs/validator/validate/ConfirmValidate.java
+++ library/src/com/throrinstudio/android/common/libs/validator/validate/ConfirmValidate.java
@@ -1,3 +1,4 @@
+
 package com.throrinstudio.android.common.libs.validator.validate;
 
 import android.content.Context;
@@ -9,44 +10,49 @@ import com.throrinstudio.android.example.validator.R;
 
 public class ConfirmValidate extends AbstractValidate {
 
-	private TextView _field1;
-	private TextView _field2;
-	private Context mContext;
-	private TextView source;
-	private int _errorMessage = R.string.validator_confirm;
-	
-	public ConfirmValidate(TextView field1, TextView field2){
-		this._field1 = field1;
-		this._field2 = field2;
-		source = _field2;
-		mContext = field1.getContext();
-	}
+    private TextView _field1;
+    private TextView _field2;
+    private Context mContext;
+    private TextView source;
+    private int _errorMessage = R.string.validator_confirm;
 
-	@Override
-	public boolean isValid(String value) {
-		if(_field1.getText().toString().length() > 0 && _field1.getText().toString().equals(_field2.getText().toString())){
-			return true;
-		}else{
-			return false;
-		}
-	}
+    public ConfirmValidate(TextView field1, TextView field2, int message) {
+        this._field1 = field1;
+        this._field2 = field2;
+        source = _field2;
+        mContext = field1.getContext();
+        _errorMessage = message;
+    }
 
+    public ConfirmValidate(TextView field1, TextView field2) {
+        this._field1 = field1;
+        this._field2 = field2;
+        source = _field2;
+        mContext = field1.getContext();
+    }
 
-	@Override
-	public String getMessages() {
-		// TODO Auto-generated method stub
-		return mContext.getString(_errorMessage);
-	}
+    @Override
+    public boolean isValid(String value) {
+        if (_field1.getText().toString().length() > 0
+                && _field1.getText().toString().equals(_field2.getText().toString())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    @Override
+    public String getMessages() {
+        return mContext.getString(_errorMessage);
+    }
 
-	@Override
-	public void addValidator(AbstractValidator validator) {
-	}
+    @Override
+    public void addValidator(AbstractValidator validator) {
+    }
 
-	@Override
-	public TextView getSource() {
-		return source;
-	}
-	
-	
+    @Override
+    public TextView getSource() {
+        return source;
+    }
+
 }
